42a7a266fe8b9cb5ab7d670eb671a90204a12a53
orisofer822 <63719889+orisofer822@users.noreply.github.com>
2022-01-28 12:53:54 +0200

    Second Coffee

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private Color colorToChange;
+    [SerializeField] private GameObject objectToColorChange;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            objectToColorChange.GetComponent<SpriteRenderer>().color = colorToChange;
+            gameObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

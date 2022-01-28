ee799e539ac0a384cf5dfe01b59da57f801ceff0
orisofer822 <63719889+orisofer822@users.noreply.github.com>
2022-01-28 17:14:36 +0200

    good times

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField] private Color colorToChange;
+
+    public void ChangeColor()
+    {
+        GetComponent<SpriteRenderer>().color = colorToChange;
+    }
+
+    //public IEnumerator ChangeColor()
+    //{
+    //    print("Entering CoRouting");
+    //    float timeStarted = Time.time;
+    //    float timeSinceStarted = Time.time - timeStarted;
+    //    print("timeSinceStarted:   " + timeSinceStarted);
+    //    float presantageComplete = timeSinceStarted / .5f;
+    //    Color currentColor = gameObject.GetComponent<SpriteRenderer>().color;
+    //    print("currentcolor:     " + currentColor);
+
+    //    while (true)
+    //    {
+    //        timeSinceStarted = Time.time - timeStarted;
+    //        presantageComplete = timeSinceStarted / .5f;
+    //        print("currentColor: " + currentColor + ", colorToChange: " + colorToChange + ", presantageComplete: " + presantageComplete);
+    //        Color newColor = Color.Lerp(currentColor, colorToChange, presantageComplete);
+    //        print("newColor: " + newColor);
+    //        currentColor = newColor;
+    //        GetComponent<SpriteRenderer>().color = currentColor;
+
+    //        if (presantageComplete >= 1f)
+    //        {
+    //            break;
+    //        }
+
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
+}
